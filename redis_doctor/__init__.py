e82df211cbabf9fe4d7ea393f688443e7610c3a7
Dario Pevec <pevec@axiros.com>
2026-06-19 11:24:37 +0000

    Pipeline

--- redis_doctor/__init__.py
+++ redis_doctor/__init__.py
@@ -1,1 +1,6 @@
-__version__ = "1.0.0"
+from importlib.metadata import PackageNotFoundError, version
+
+try:
+    __version__ = version("redis-doctor")
+except PackageNotFoundError:  # running from a source checkout without an install
+    __version__ = "0.0.0+unknown"
